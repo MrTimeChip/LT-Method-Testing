fb49eccb3b055ec4e7a56a85984237856a1bfff5
Данил Панков <mistertimechip@mail.ru>
2020-10-30 13:53:00 +0500

    Added simple cases using casegenerator.

---
+++ cases.py
@@ -0,0 +1,74 @@
+import casegenerator
+
+
+def linear_normal():
+    x, y = casegenerator.generate_values(amount=1800).with_random().extract()
+    return x, y
+
+
+def linear_with_outlier():
+    x, y = casegenerator\
+        .generate_values(amount=1800)\
+        .with_random()\
+        .extract()
+    x_anom, y_anom = casegenerator\
+        .empty()\
+        .using(x, y)\
+        .with_outlier()\
+        .extract()
+    return x, y, x_anom, y_anom
+
+
+def linear_with_extreme_outliers():
+    x, y = casegenerator\
+        .generate_values(amount=1800)\
+        .with_random()\
+        .extract()
+    x_anom, y_anom = casegenerator\
+        .empty()\
+        .using(x, y)\
+        .with_outlier(extreme_multiplier=0.7)\
+        .extract()
+    x_final, y_final = casegenerator\
+        .empty()\
+        .using(x_anom, y_anom)\
+        .with_outlier(extreme_multiplier=1.2)\
+        .extract()
+    return x, y, x_final, y_final
+
+
+def linear_with_ramp_up():
+    x, y = casegenerator\
+        .generate_values(amount=1800)\
+        .extract()
+    x_anom, y_anom = casegenerator\
+        .empty()\
+        .using(x, y)\
+        .with_step(after=700, over=300, diff=10)\
+        .with_random()
+    return x, y, x_anom, y_anom
+
+
+def linear_with_ramp_down():
+    x, y = casegenerator\
+        .generate_values(amount=1800)\
+        .extract()
+    x_anom, y_anom = casegenerator\
+        .empty()\
+        .using(x, y)\
+        .with_step(after=700, over=300, diff=-10)\
+        .with_random()
+    return x, y, x_anom, y_anom
+
+
+def linear_with_sudden_step():
+    x, y = casegenerator\
+        .generate_values(amount=1800)\
+        .extract()
+    x_anom, y_anom = casegenerator\
+        .empty()\
+        .using(x, y)\
+        .with_step(after=0, over=1, diff=20)\
+        .with_random()
+    return x, y, x_anom, y_anom
+
