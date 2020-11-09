69fe3808fb29489d32d5501b788f8c8f950692a1
Данил Панков <mistertimechip@mail.ru>
2020-11-09 17:31:39 +0500

    New generator - periodic values (sin-based) Outliers changes: - Outliers now change the original 'y' value, instead of creating a new one. - Outliers now have a support of negative multipliers. - Fixed bug with randomly changing the value, even if there is no outlier needed.

--- casegenerator.py
+++ casegenerator.py
@@ -17,15 +17,18 @@ class AnomalyContext:
         min_y = math.trunc(min(new_y))
         max_y = math.trunc(max(new_y))
 
+        sgn = math.copysign(1, extreme_multiplier)
+        extreme_multiplier = abs(extreme_multiplier)
+
         avg = (min_y + max_y) // 2
+
+        min_seed = math.trunc((avg * 1.7 - avg / 2) * extreme_multiplier)
+        max_seed = math.trunc((avg + avg / 4) * extreme_multiplier)
+
         amount = len(new_y)
         for i in range(amount):
             if random.random() > avg_impact:
-                min_seed = math.trunc((avg * 1.7 - avg / 2) * extreme_multiplier)
-                max_seed = math.trunc((avg + avg / 4) * extreme_multiplier)
-                new_y[i] = random.randint(min_seed, max_seed)
-            else:
-                new_y[i] = random.randint(min_y, max_y)
+                new_y[i] = new_y[i] + sgn * random.randint(min_seed, max_seed)
         return AnomalyContext(new_x, new_y)
 
     def with_step(self, after=500, over=300, diff=5):
@@ -45,7 +48,7 @@ class AnomalyContext:
         new_y = copy.copy(self.y)
         amount = len(new_x)
         for i in range(amount):
-            new_y[i] *= random.randint(800, 1000) / 1000
+            new_y[i] *= random.randint(900, 1100)/1000
         return AnomalyContext(new_x, new_y)
 
     def extract(self):
@@ -57,6 +60,18 @@ def generate_values(min_value=60, max_value=65, amount=1500):
     y = []
     for i in range(amount):
         value = random.randint(min_value, max_value)
+        x.append(i)
+        y.append(value)
+    return AnomalyContext(x, y)
+
+
+def generate_periodic_values(min_value=25, max_value=75, amount=1500,
+                             period_multiplier=1):
+    x = []
+    y = []
+    half_range = (max_value - min_value) / 2
+    for i in range(amount):
+        value = min_value + half_range + math.sin(period_multiplier * i) * half_range
         x.append(i)
         y.append(value)
     return AnomalyContext(x, y)
