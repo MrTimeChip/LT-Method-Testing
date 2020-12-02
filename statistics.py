e32ae5ea509736c687cfc3653a078b2f6e6c4a5a
Данил Панков <mistertimechip@mail.ru>
2020-12-02 13:23:06 +0500

    Added KS tests: default and windowed

--- statistics.py
+++ statistics.py
@@ -1,6 +1,6 @@
 import numpy
 from outliers import smirnov_grubbs as grubbs
-from scipy.stats import zscore, iqr, ttest_ind, mannwhitneyu
+from scipy.stats import zscore, iqr, ttest_ind, mannwhitneyu, ks_2samp
 
 
 def empirical_rule(y, y_anom):
@@ -92,6 +92,32 @@ def mann_whitney_u_test_window(y, y_anom):
         values_y = y[right_edge - window:right_edge]
         values_y_anom = y_anom[right_edge - window:right_edge]
         t, p = mannwhitneyu(values_y, values_y_anom)
+        if p < alpha:
+            ind = right_edge - window
+            anomalies.append((ind, y_anom[ind]))
+        right_edge += step
+    return anomalies
+
+
+def kolomogorov_smirnov_test(y, y_anom):
+    alpha = 0.05
+    t, p = ks_2samp(y, y_anom)
+    if p < alpha:
+        return [(0, y_anom[0])]
+    return []
+
+
+def kolomogorov_smirnov_test_window(y, y_anom):
+    anomalies = []
+    amount = len(y_anom)
+    window = amount // 30
+    step = window // 2
+    right_edge = window
+    alpha = 0.05
+    while right_edge < amount:
+        values_y = y[right_edge - window:right_edge]
+        values_y_anom = y_anom[right_edge - window:right_edge]
+        t, p = ks_2samp(values_y, values_y_anom)
         if p < alpha:
             ind = right_edge - window
             anomalies.append((ind, y_anom[ind]))
