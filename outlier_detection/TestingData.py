466f7c417704bb064dcb0fab480357509885b4f6
Данил Панков <mistertimechip@mail.ru>
2021-02-21 19:56:17 +0500

    Added TestingData object: - Used for abstraction over test data with many instances. (like different sets of tests over one service version)

---
+++ outlier_detection/TestingData.py
@@ -0,0 +1,16 @@
+from outlier_detection.OutliersInfo import OutliersInfo
+
+
+class TestingData:
+    def __init__(self, data_instances):
+        self.__data_instances = data_instances
+        self.__outliers = OutliersInfo(data_instances)
+
+    def find_outliers(self):
+        self.__outliers.analyze_data()
+
+    def is_outlier_among_data(self, info_point):
+        self.__outliers.is_outlier(info_point)
+
+    def are_outliers_among_data(self, info_points):
+        self.__outliers.are_outliers(info_points)
