5a22b2ab86243db9721eaa78fcfbcfd19da784d6
Данил Панков <mistertimechip@mail.ru>
2021-02-27 14:37:39 +0500

    Methods for outliers count comparison.

--- outlier_detection/TestingData.py
+++ outlier_detection/TestingData.py
@@ -17,3 +17,9 @@ class TestingData:
 
     def get_all_outliers(self):
         return self.__outliers.get_all_outliers()
+
+    def is_outliers_count_different(self, count_info):
+        return self.__outliers.is_outlier_count_different(count_info)
+
+    def get_outlier_count_info(self):
+        return self.__outliers.get_outlier_count_info()
