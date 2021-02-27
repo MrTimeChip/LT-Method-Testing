d4023517ac23e1a322d440d4edb718a743216c96
Данил Панков <mistertimechip@mail.ru>
2021-02-27 14:37:02 +0500

    Added methods for outliers count comparison: Pretty basic, just compares if new count exceeds a certain limit, which is max + deviation. Could work for now.

--- outlier_detection/OutliersInfo.py
+++ outlier_detection/OutliersInfo.py
@@ -16,6 +16,9 @@ class OutliersInfo:
     def get_all_outliers(self):
         return self.__all_outliers
 
+    def get_outlier_count_info(self):
+        return self.__count_deviation, self.__min_count, self.__max_count
+
     def is_outlier(self, info_point, treat_original_as_normal=True):
         return abs(info_point[1] - self.__outliers_avg) > 3 * self.__outliers_std
 
@@ -26,6 +29,9 @@ class OutliersInfo:
                 result.append(info_point)
         return result
 
+    def is_outlier_count_different(self, outlier_count_info):
+        return self.__max_count + self.__count_deviation < outlier_count_info[2]
+
     def analyze_data(self):
         amounts = []
         for data in self.__data_instances:
@@ -35,7 +41,8 @@ class OutliersInfo:
             amounts.append(len(outliers))
         self.__min_count = min(amounts)
         self.__max_count = max(amounts)
-        self.__count_deviation = tstd(amounts)
+        if len(amounts) > 1:
+            self.__count_deviation = tstd(amounts)
 
         self.__outliers_std = tstd(self.__all_outliers)[1]
         self.__outliers_avg = tmean(self.__all_outliers)
