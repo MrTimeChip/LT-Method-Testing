a3f0c075178961ac6c2cf820a3e5b3af264844f6
Данил Панков <mistertimechip@mail.ru>
2021-03-17 14:03:32 +0500

    Added shift points to TestResult

--- TestResult.py
+++ TestResult.py
@@ -4,3 +4,7 @@ class TestResult:
         self.anomalies = anomalies
         self.is_outliers_count_exceeded = False
         self.is_outliers_density_exceeded = False
+        self.shift_point = None
+
+    def add_shift_point(self, point):
+        self.shift_point = point
