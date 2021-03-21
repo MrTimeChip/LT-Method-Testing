ff5f2b40101f7d0ab20675c33ab8b455c0b0a2a3
Данил Панков <mistertimechip@mail.ru>
2021-03-22 00:12:03 +0500

    Instead of using "exceeded" now just uses difference between original and new.

--- TestResult.py
+++ TestResult.py
@@ -1,8 +1,8 @@
 class TestResult:
     def __init__(self, anomalies):
         self.anomalies = anomalies
-        self.is_outliers_count_exceeded = False
-        self.is_outliers_density_exceeded = False
+        self.outliers_count_compared = 0
+        self.outliers_density_compared = 0
         self.shift_point = None
 
     def add_shift_point(self, point):
